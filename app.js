aa71db89929b9027aaf7f8c1af3b3f4e40cbbb6f
Ryoya Tamura <simochee@outlook.jp>
2016-10-20 22:34:40 +0900

    アプリケーション側のAPI完成

--- app.js
+++ app.js
@@ -7,19 +7,13 @@ const bodyParser = require('body-parser');
 const apis = require('./routes/apis');
 
 const app = express();
-app.listen('3000');
+app.listen('4301', () => {
+	console.log('Server running at localhost:4301');
+});
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// 404
-app.use((req, res, next) => {
-	res.status(404);
-	res.send({
-		status: 'error'
-	});
-});
 
 // from Services
 app.get('/:year/:month', apis.menu);
@@ -29,3 +23,11 @@ app.get('/:year/:month/:word', apis.search);
 app.post('/:year/:month', apis.update);
 app.put('/:year/:month', apis.create);
 app.delete('/:year/:month', apis.delete);
+
+// 404
+app.use((req, res, next) => {
+	res.status(404);
+	res.send({
+		status: '404'
+	});
+});
--- routes/apis.js
+++ routes/apis.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('menu');
+const db = new sqlite3.Database('db.sqlite3');
 
 const zero = (num, lv = 2) => {
 	return ('0000000000' + num).slice(-lv);
@@ -14,34 +14,87 @@ module.exports = {
 				status: null,
 				items: []
 			};
-			db.each(`SELECT * FROM menu WHERE date BETWEEN ${req.year}${zero(req.month)}01 AND ${req.year}${zero(req.month)}31;`, (err, row) => {
+			db.each(`SELECT * FROM menu WHERE date BETWEEN ${req.params.year}${zero(req.params.month)}01 AND ${req.params.year}${zero(req.params.month)}31;`, (err, row) => {
 				if(err) {
+					res.status(404);
 					res.send({
 						status: 'error'
 					});
 				}
-				menu.items.push({
-					date: row.date,
-					mMain: row.m_main,
-					mSide: row.m_side,
-					lMain: row.l_main,
-					lSide: row.l_side,
-					dMain: row.d_main,
-					dSide: row.d_side
-				});
+				if(row.active === 1) {
+					menu.items.push({
+						date: row.date,
+						m_main: row.m_main,
+						m_side: row.m_side,
+						l_main: row.l_main,
+						l_side: row.l_side,
+						d_main: row.d_main,
+						d_side: row.d_side
+					});
+				}
+			}, (err, count) => {
+				if(err) {
+					res.status(404);
+					res.send({
+						status: 'error'
+					})
+				} else {
+					res.status(200);
+					if(count === 0) {
+						res.send({
+							status: 'notfound'
+						});
+					} else {
+						menu.status = 'success';
+						res.send(menu);
+					}
+				}
 			});
-			menu.status = 'OK';
-			res.send(menu);
 		});
-		db.close();
 	},
 	search: (req, res) => {
 		db.serialize(() => {
-			let result = {
+			let results = {
 				status: null,
 				items: []
 			}
-			db.each(`SELECT * FROM menu WHERE `)
+			console.log(req.params.word)
+			db.each(`SELECT * FROM menu WHERE (date BETWEEN ${req.params.year}${zero(req.params.month)}01 AND ${req.params.year}${zero(req.params.month)}31) AND (m_main LIKE "%${req.params.word}%" OR l_main LIKE "%${req.params.word}%" OR d_main LIKE "%${req.params.word}%" OR m_side LIKE "%${req.params.word}%" OR l_side LIKE "%${req.params.word}%" OR d_side LIKE "%${req.params.word}%")`, (err, row) => {
+				if(err) {
+					res.status(404);
+					res.send({
+						status: 'error'
+					});
+				}
+				if(row.active === 1) {
+					results.items.push({
+						date: row.date,
+						m_main: row.m_main,
+						m_side: row.m_side,
+						l_main: row.l_main,
+						l_side: row.l_side,
+						d_main: row.d_main,
+						d_side: row.d_side
+					});
+				}
+			}, (err, count) => {
+				if(err) {
+					res.status(404);
+					res.send({
+						status: 'error'
+					});
+				} else {
+					res.status(200);
+					if(count === 0) {
+						res.send({
+							status: 'notfound'
+						});
+					} else {
+						results.status = 'success';
+						res.send(results);
+					}
+				}
+			});
 		});
 	},
 	update: (req, res) => {
